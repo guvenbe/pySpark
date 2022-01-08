965b37de598d3301f82afb2b6fe6df5f5077d3ab
Bora Guven <guvenbe@gmail.com>
2022-01-08 00:43:53 -0500

    files updated fixed

--- total-spent-by-customer-sortedBG.py
+++ total-spent-by-customer-sortedBG.py
@@ -13,9 +13,11 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile("file:///home/vagrant/PycharmProjects/pySpark/ml-100k/customer-orders.csv")
 rdd = lines.map(parseline)
-totalsByCustomer =rdd.reduceByKey(lambda x, y: x+ y)
-rdd.map(lambda x: (x[1], x[0])).sortByKey
-results = totalsByCustomer.collect()
+totalsByCustomer =rdd.reduceByKey(lambda x, y: x + y)
+flipped = totalsByCustomer.map(lambda x: (x[1], x[0]))
+totalByCustomerSorted = flipped.sortByKey()
+sortedResults = totalByCustomerSorted.collect()
 
-for result in results:
-    print(str(result[0]) + str("\t{:.2f}".format(result[1])))
+for result in sortedResults:
+    print(str("{:.2f}".format(result[0]) +"\t\t"+ str(result[1])))
+    # print(result)
